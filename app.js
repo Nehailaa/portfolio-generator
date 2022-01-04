de19ba8e53bcd0c00b9f4a5729ea4b8049a1db92
Nehailaa <93628865+Nehailaa@users.noreply.github.com>
2022-01-04 08:53:15 -0600

    fs function

--- app.js
+++ app.js
@@ -1,18 +1,68 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
-    //This ...
-    for (let i = 0; i < profileDataArr.length; i +=1) {
-  console.length(profileDataArr[i]);
-    }
-    console.log('================');
+const profileDataArgs = process.argv.slice(2);
 
-      // Is the same as this...
-profileDataArr.forEach(profileItem => console.log(profileItem));
+const [name, github] = profileDataArgs;
 
+const generatePage = (name, github) => {
+  return `
+  <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
+
+  <body>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+  </body>
+  </html>
+  `;
 };
 
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
-printProfileData(profileDataArgs);
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Notice the lack of parentheses around the `profileDataArr` parameter?
+// const printProfileData = profileDataArr => {
+//     //This ...
+//     for (let i = 0; i < profileDataArr.length; i +=1) {
+//   console.length(profileDataArr[i]);
+//     }
+//     console.log('================');
+
+//       // Is the same as this...
+// profileDataArr.forEach(profileItem => console.log(profileItem));
+
+// };
+
+
+// printProfileData(profileDataArgs);
